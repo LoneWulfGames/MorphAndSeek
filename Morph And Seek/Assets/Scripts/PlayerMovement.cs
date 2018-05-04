d147a93f04170ed02f2329c04719510184c37b85
LoneWulfGames <adamb@biastudios.com>
2018-05-03 21:39:37 -0400

    PlayerMovement
    Created a basic player with movement.
    Prefabbed it.
    Saved Scene as DevScene for doing development and testing.

---
+++ Morph And Seek/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    //physics
+    Rigidbody rb;
+    //movement
+    float walk, turn;
+    public float walkSpeed, turnSpeed;
+    //jumping
+    float jumpForce;
+    bool canJump;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponentInChildren<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        walk = Input.GetAxis("Vertical");
+        turn = Input.GetAxis("Horizontal");
+
+	}
+
+    private void FixedUpdate()
+    {
+        if(walk!=0 || turn != 0)
+        {
+            PlayerMove();
+        }
+    }
+
+    private void PlayerMove()
+    {
+        transform.Translate(Vector3.forward * walk * walkSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * turn * turnSpeed * Time.deltaTime);
+    }
+}
