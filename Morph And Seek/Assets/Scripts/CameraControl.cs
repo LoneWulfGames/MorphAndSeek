600c7beb0f8a11d9a87ae9d549a5874685bb5ca2
Adam Barse <adamb@biastudios.com>
2018-05-10 12:58:01 -0400

    Porper switching morphing

---
+++ Morph And Seek/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public Vector3 camPOS;
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        //check if the local player
+        SetPlayer();
+        transform.SetParent(player.transform);
+	}
+    void SetPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        SetPOV();
+    }
+    void SetPOV()
+    {
+        transform.position = player.transform.position + camPOS;
+    }
+}
---
+++ Morph And Seek/Assets/Scripts/MorphManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+public class MorphManager : MonoBehaviour {
+    public GameObject playerBody,morphBody;
+
+    public void Morph(GameObject prop)
+    {
+        if (playerBody.activeInHierarchy)
+        {  playerBody.SetActive(false); }
+        else
+        {  Destroy(morphBody);}
+        GameObject newMorphBody;
+        newMorphBody = Instantiate(prop, transform.position, Quaternion.identity, transform) as GameObject;
+        morphBody = newMorphBody;
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                print("hit a " + hit.transform.name);
+                if (hit.transform.CompareTag("Prop"))
+                { Morph(hit.transform.GetComponent<PropHolder>().propModel); }
+            }
+        }
+    }
+}
